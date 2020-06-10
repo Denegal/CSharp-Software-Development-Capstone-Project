4ba8bf398f8bdaf5550cbae3f05f32bc157dd504
Denegal <korivain@gmail.com>
2020-06-10 13:17:45 -0500

    Created backend database and backend logic projects.

---
+++ Backend_Logic/Logging.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Logic
+{
+
+    // The Logging class is a static class with functions used to read/write log messages and debugging information to file
+    public static class Logging
+    {
+
+        // This method is simply used to write a message to a log file
+        // It handles timestamping as well.
+        public static void AddToLog(string logfile, string message)
+        {
+            using (StreamWriter file = new StreamWriter(logfile, true))
+            {
+                file.WriteLine(DateTime.UtcNow.ToString() + " > " + message);
+            }
+        }
+    }
+}
---
+++ Backend_Logic/Utils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Logic
+{
+    // The Utils class is a static class with helper functions useful to database operations and data manipulation
+    public static class Utils
+    {
+        //Hashing function to translate a string into it's SHA1 hash
+        public static string GetSha1(String input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var result = new StringBuilder(hash.Length * 2);
+
+                foreach (var by in hash)
+                {
+                    result.Append(by.ToString("x2"));
+                }
+
+                return result.ToString();
+            }
+        }
+    }
+}
