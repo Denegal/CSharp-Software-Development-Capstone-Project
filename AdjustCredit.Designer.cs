8f91844d956bb241015cc2b6a0d63d4b2e21da68
Denegal <korivain@gmail.com>
2020-07-05 09:45:58 -0500

    Adjusted Client credit form per customer recomendation

--- AdjustCredit.Designer.cs
+++ AdjustCredit.Designer.cs
@@ -32,16 +32,18 @@
             this.button_Save = new System.Windows.Forms.Button();
             this.button_Cancel = new System.Windows.Forms.Button();
             this.group_Client = new System.Windows.Forms.GroupBox();
-            this.label_NewPass2 = new System.Windows.Forms.Label();
+            this.textbox_Adjustment = new System.Windows.Forms.TextBox();
+            this.label_NewBalance = new System.Windows.Forms.Label();
+            this.label_CurrentBalance = new System.Windows.Forms.Label();
+            this.combo_Client = new System.Windows.Forms.ComboBox();
+            this.label_Cash = new System.Windows.Forms.Label();
             this.label_Client = new System.Windows.Forms.Label();
             this.textbox_LName = new System.Windows.Forms.TextBox();
             this.label_Title = new System.Windows.Forms.Label();
-            this.combo_Client = new System.Windows.Forms.ComboBox();
-            this.label_CurrentBalance = new System.Windows.Forms.Label();
-            this.button_Remove = new System.Windows.Forms.Button();
-            this.button_Add = new System.Windows.Forms.Button();
-            this.label_NewBalance = new System.Windows.Forms.Label();
-            this.textbox_Adjustment = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.group_Client.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,7 +56,7 @@
             this.button_Save.FlatAppearance.BorderColor = System.Drawing.SystemColors.MenuHighlight;
             this.button_Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_Save.ForeColor = System.Drawing.Color.White;
-            this.button_Save.Location = new System.Drawing.Point(50, 307);
+            this.button_Save.Location = new System.Drawing.Point(50, 383);
             this.button_Save.Name = "button_Save";
             this.button_Save.Size = new System.Drawing.Size(178, 87);
             this.button_Save.TabIndex = 13;
@@ -70,7 +72,7 @@
             this.button_Cancel.FlatAppearance.BorderColor = System.Drawing.SystemColors.MenuHighlight;
             this.button_Cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_Cancel.ForeColor = System.Drawing.Color.White;
-            this.button_Cancel.Location = new System.Drawing.Point(259, 307);
+            this.button_Cancel.Location = new System.Drawing.Point(259, 383);
             this.button_Cancel.Name = "button_Cancel";
             this.button_Cancel.Size = new System.Drawing.Size(178, 87);
             this.button_Cancel.TabIndex = 12;
@@ -79,33 +81,81 @@
             // 
             // group_Client
             // 
+            this.group_Client.Controls.Add(this.label1);
+            this.group_Client.Controls.Add(this.textBox1);
+            this.group_Client.Controls.Add(this.button1);
+            this.group_Client.Controls.Add(this.button2);
             this.group_Client.Controls.Add(this.textbox_Adjustment);
             this.group_Client.Controls.Add(this.label_NewBalance);
-            this.group_Client.Controls.Add(this.button_Remove);
-            this.group_Client.Controls.Add(this.button_Add);
             this.group_Client.Controls.Add(this.label_CurrentBalance);
             this.group_Client.Controls.Add(this.combo_Client);
-            this.group_Client.Controls.Add(this.label_NewPass2);
+            this.group_Client.Controls.Add(this.label_Cash);
             this.group_Client.Controls.Add(this.label_Client);
             this.group_Client.Controls.Add(this.textbox_LName);
             this.group_Client.Location = new System.Drawing.Point(7, 73);
             this.group_Client.Name = "group_Client";
-            this.group_Client.Size = new System.Drawing.Size(478, 228);
+            this.group_Client.Size = new System.Drawing.Size(478, 289);
             this.group_Client.TabIndex = 11;
             this.group_Client.TabStop = false;
             // 
-            // label_NewPass2
+            // textbox_Adjustment
             // 
-            this.label_NewPass2.AutoSize = true;
-            this.label_NewPass2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_NewPass2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label_NewPass2.Location = new System.Drawing.Point(38, 174);
-            this.label_NewPass2.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
-            this.label_NewPass2.Name = "label_NewPass2";
-            this.label_NewPass2.Size = new System.Drawing.Size(136, 25);
-            this.label_NewPass2.TabIndex = 33;
-            this.label_NewPass2.Text = "Adjustment:";
-            this.label_NewPass2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.textbox_Adjustment.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbox_Adjustment.Location = new System.Drawing.Point(164, 228);
+            this.textbox_Adjustment.MaxLength = 3;
+            this.textbox_Adjustment.Name = "textbox_Adjustment";
+            this.textbox_Adjustment.Size = new System.Drawing.Size(101, 31);
+            this.textbox_Adjustment.TabIndex = 56;
+            this.textbox_Adjustment.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textbox_Adjustment_KeyPress);
+            // 
+            // label_NewBalance
+            // 
+            this.label_NewBalance.AutoSize = true;
+            this.label_NewBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_NewBalance.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label_NewBalance.Location = new System.Drawing.Point(71, 121);
+            this.label_NewBalance.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
+            this.label_NewBalance.Name = "label_NewBalance";
+            this.label_NewBalance.Size = new System.Drawing.Size(233, 25);
+            this.label_NewBalance.TabIndex = 55;
+            this.label_NewBalance.Text = "New Credit Balance: ";
+            this.label_NewBalance.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_CurrentBalance
+            // 
+            this.label_CurrentBalance.AutoSize = true;
+            this.label_CurrentBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_CurrentBalance.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label_CurrentBalance.Location = new System.Drawing.Point(38, 86);
+            this.label_CurrentBalance.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
+            this.label_CurrentBalance.Name = "label_CurrentBalance";
+            this.label_CurrentBalance.Size = new System.Drawing.Size(266, 25);
+            this.label_CurrentBalance.TabIndex = 52;
+            this.label_CurrentBalance.Text = "Current Credit Balance: ";
+            this.label_CurrentBalance.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // combo_Client
+            // 
+            this.combo_Client.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.combo_Client.FormattingEnabled = true;
+            this.combo_Client.Location = new System.Drawing.Point(188, 25);
+            this.combo_Client.Name = "combo_Client";
+            this.combo_Client.Size = new System.Drawing.Size(277, 33);
+            this.combo_Client.TabIndex = 51;
+            this.combo_Client.SelectionChangeCommitted += new System.EventHandler(this.combo_Client_SelectionChangeCommitted);
+            // 
+            // label_Cash
+            // 
+            this.label_Cash.AutoSize = true;
+            this.label_Cash.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Cash.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label_Cash.Location = new System.Drawing.Point(6, 232);
+            this.label_Cash.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
+            this.label_Cash.Name = "label_Cash";
+            this.label_Cash.Size = new System.Drawing.Size(152, 25);
+            this.label_Cash.TabIndex = 33;
+            this.label_Cash.Text = "Amount Paid:";
+            this.label_Cash.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label_Client
             // 
@@ -141,79 +191,53 @@
             this.label_Title.Text = "Adjust Client Credit";
             this.label_Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // combo_Client
+            // textBox1
             // 
-            this.combo_Client.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.combo_Client.FormattingEnabled = true;
-            this.combo_Client.Location = new System.Drawing.Point(188, 25);
-            this.combo_Client.Name = "combo_Client";
-            this.combo_Client.Size = new System.Drawing.Size(277, 33);
-            this.combo_Client.TabIndex = 51;
-            this.combo_Client.SelectionChangeCommitted += new System.EventHandler(this.combo_Client_SelectionChangeCommitted);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(164, 172);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 31);
+            this.textBox1.TabIndex = 59;
             // 
-            // label_CurrentBalance
+            // button1
             // 
-            this.label_CurrentBalance.AutoSize = true;
-            this.label_CurrentBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_CurrentBalance.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label_CurrentBalance.Location = new System.Drawing.Point(38, 86);
-            this.label_CurrentBalance.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
-            this.label_CurrentBalance.Name = "label_CurrentBalance";
-            this.label_CurrentBalance.Size = new System.Drawing.Size(266, 25);
-            this.label_CurrentBalance.TabIndex = 52;
-            this.label_CurrentBalance.Text = "Current Credit Balance: ";
-            this.label_CurrentBalance.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(355, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 33);
+            this.button1.TabIndex = 58;
+            this.button1.Text = "Remove";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // button_Remove
+            // button2
             // 
-            this.button_Remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_Remove.Location = new System.Drawing.Point(361, 171);
-            this.button_Remove.Name = "button_Remove";
-            this.button_Remove.Size = new System.Drawing.Size(104, 33);
-            this.button_Remove.TabIndex = 54;
-            this.button_Remove.Text = "Remove";
-            this.button_Remove.UseVisualStyleBackColor = true;
-            this.button_Remove.Click += new System.EventHandler(this.button_Remove_Click);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(271, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 33);
+            this.button2.TabIndex = 57;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // button_Add
+            // label1
             // 
-            this.button_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_Add.Location = new System.Drawing.Point(277, 171);
-            this.button_Add.Name = "button_Add";
-            this.button_Add.Size = new System.Drawing.Size(78, 33);
-            this.button_Add.TabIndex = 53;
-            this.button_Add.Text = "Add";
-            this.button_Add.UseVisualStyleBackColor = true;
-            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
-            // 
-            // label_NewBalance
-            // 
-            this.label_NewBalance.AutoSize = true;
-            this.label_NewBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_NewBalance.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label_NewBalance.Location = new System.Drawing.Point(71, 121);
-            this.label_NewBalance.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
-            this.label_NewBalance.Name = "label_NewBalance";
-            this.label_NewBalance.Size = new System.Drawing.Size(233, 25);
-            this.label_NewBalance.TabIndex = 55;
-            this.label_NewBalance.Text = "New Credit Balance: ";
-            this.label_NewBalance.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // textbox_Adjustment
-            // 
-            this.textbox_Adjustment.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textbox_Adjustment.Location = new System.Drawing.Point(170, 171);
-            this.textbox_Adjustment.MaxLength = 3;
-            this.textbox_Adjustment.Name = "textbox_Adjustment";
-            this.textbox_Adjustment.Size = new System.Drawing.Size(101, 31);
-            this.textbox_Adjustment.TabIndex = 56;
-            this.textbox_Adjustment.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textbox_Adjustment_KeyPress);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(-2, 175);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 60;
+            this.label1.Text = "Class Credits:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // AdjustCredit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(495, 412);
+            this.ClientSize = new System.Drawing.Size(495, 488);
             this.ControlBox = false;
             this.Controls.Add(this.button_Save);
             this.Controls.Add(this.button_Cancel);
@@ -235,15 +259,17 @@
         private System.Windows.Forms.Button button_Save;
         private System.Windows.Forms.Button button_Cancel;
         private System.Windows.Forms.GroupBox group_Client;
-        private System.Windows.Forms.Label label_NewPass2;
+        private System.Windows.Forms.Label label_Cash;
         private System.Windows.Forms.Label label_Client;
         private System.Windows.Forms.TextBox textbox_LName;
         private System.Windows.Forms.Label label_Title;
         private System.Windows.Forms.Label label_CurrentBalance;
         private System.Windows.Forms.ComboBox combo_Client;
         private System.Windows.Forms.Label label_NewBalance;
-        private System.Windows.Forms.Button button_Remove;
-        private System.Windows.Forms.Button button_Add;
         private System.Windows.Forms.TextBox textbox_Adjustment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
